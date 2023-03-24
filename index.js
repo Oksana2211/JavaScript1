4b677418fd9fc08a798b98c300deea93069fb549
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-03-24 14:36:25 +0300

    add DragDrop

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Первый проет Слайдер
 const slides = document.querySelectorAll(".slide");
 
 for (const slide of slides) {
@@ -11,3 +12,41 @@ function clearActiveClasses() {
     slide.classList.remove("active");
   });
 }
+//Первый проет Слайдер
+
+//Проект 2 Drag & Drop
+const item = document.querySelector('.item')
+const placeholders = document.querySelectorAll('.placeholder')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+for (const placeholder of placeholders){
+  placeholder.addEventListener('dragover', dragover)
+  placeholder.addEventListener('dragenter', dragenter)
+  placeholder.addEventListener('dragleav', dragleav)
+  placeholder.addEventListener('drop', drop)
+}
+
+function dragstart(event) {
+  event.target.classList.add('hold')
+  setTimeout(()=> event.target.classList.add('hide'),0)
+}
+
+function dragend(event) { event.target.classList.remove('hold','hide')
+}
+
+function dragover(event){
+  event.preventDefault()
+}
+function dragenter(event){
+  event.target.classList.add('hovered')
+}
+function dragleav(event){
+  event.target.classList.remove('hovered')
+}
+function drop(event) {
+  event.target.classList.remove('hovered')
+  event.target.append(item)
+}
+//Проект 2 Drag & Drop
